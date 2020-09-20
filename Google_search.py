c1e563ef8e281a5fb1da50f472838eba1bcdcc18
tserber <50498885+tserber@users.noreply.github.com>
2020-09-20 15:11:50 +0300

    Add files via upload

--- Google_search.py
+++ Google_search.py
@@ -2,45 +2,50 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from docx import Document
 import requests
-import urllib.request
+from urllib.request import Request, urlopen
 
 
 class Google_search():
-    def __init__(self, search, re_finds):
+    def __init__(self, search, sought):
         self.search = search
-        self.re_finds = re_finds
+        self.sought = sought
 
-    def get_html(self):
-        search_input = self.search.replace(" ","+")
-        respons = requests.get('https://www.google.com/search?q='+search_input)
+    def get_html_soup(self, url = False):
         # print(respons.text)
+        if url:
+            respons = requests.get(url)
+        else:
+            search_input = self.search.replace(" ","+")
+            respons = requests.get('https://www.google.com/search?q='+search_input)
         soup = BeautifulSoup(respons.text, 'lxml')
         # print(soup.prettify())
         # url = f'https://www.google.com/search?q={self.search}'
         # r = requests.get(url)
         # return r.text
         return soup
-
     def get_headers(self):
-        return self.get_html().find_all('h3')
+        return self.get_html_soup().find_all('h3')
 
-    def get_a_from_names(self):
+    def get_href_from_headers(self):
         list_of_names=[]
         for name in self.get_headers():
-            url= 'https://www.google.com'+ name.parent.attrs['href']
-            response = urllib.request.urlopen(url)
-            res = response.geturl()
-            list_of_names.append(res)
+            if name.parent.attrs: # if h3 had attributes then do
+                url= 'https://www.google.com'+ name.parent.attrs['href']
+                req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+                response=urlopen(req)
+                res = response.geturl()
+                list_of_names.append(res)
+            else:                 # without h3 attributes pass
+                pass
+
         return list_of_names
 
-
-
-    def get_href_google_results(self):
-        names= self.get_headers()
-        list_of_names=[]
-        for name in names:
-            list_of_names.append(name.parent)
-        return list_of_names
+    # def get_href_google_results(self):
+    #     names= self.get_headers()
+    #     list_of_names=[]
+    #     for name in names:
+    #         list_of_names.append(name.parent)
+    #     return list_of_names
 
     def get_text_google_results(self):
         names = self.get_headers()
@@ -49,9 +54,24 @@ class Google_search():
             res.append(name.text)
         return res
 
+    def get_sought_from_url(self):
+        self.get_href_from_headers()
+        for url in self.get_href_from_headers():
+            list_of_p = self.get_html_soup(url).find_all('p')
+            print(url)
+            for p in list_of_p:
+                if any(word in p.text for word in self.sought): # search in every p tag our sought and gives it back
+                    print(p.text)
+            print('\n'*5)
+
+
+
+
+
 def main():
-    i=Google_search('krip r','Киев')
-    print(i.get_a_from_names())
+    i=Google_search('kiev',['kiev','киев','Kiev'])
+    # print(i.get_href_from_headers())
+    i.get_sought_from_url()
 
 
     # for i in i.get_href_google_results():
@@ -61,7 +81,6 @@ def main():
 # for name in names:
 #     list_of_names.append(name.parent)
 # return list_of_names
-
 
 if __name__ == '__main__':
     main()
